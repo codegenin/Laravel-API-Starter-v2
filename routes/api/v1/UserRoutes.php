0dd8167478797f818404c991105faee06a904542
James Puro <james.louie.puro@gmail.com>
2018-06-02 12:47:03 +0800

    Fix user profile route and controller

--- routes/api/v1/UserRoutes.php
+++ routes/api/v1/UserRoutes.php
@@ -7,6 +7,8 @@ $api = app(Router::class);
 $api->version('v1', function (Router $api) {
     
     $api->group(['prefix' => 'user'], function (Router $api) {
+    
+        $api->get('me', 'App\\ACME\\Api\\V1\\User\\Controllers\\UserController@me');
         
         $api->post('role-update', 'App\\ACME\\Api\\V1\\User\\Controllers\\RoleController@updateUserRole');
         
