9a68d728ffbf3cb9229ddf33e09b19a94229768c
James Puro <james.louie.puro@gmail.com>
2018-06-12 04:10:16 +0800

    Fix resource

--- app/ACME/Api/V1/Category/Resource/CategoryResource.php
+++ app/ACME/Api/V1/Category/Resource/CategoryResource.php
@@ -25,7 +25,7 @@ class CategoryResource extends JsonResource
             'name'        => $this->name,
             'description' => $this->description,
             'public'      => ($this->is_public == 1) ? 'Yes' : 'No',
-            'covers'      => $this->getMedialUrls($this, $this->slug),
+            'covers'      => $this->getMedialUrls($this, 'category'),
             #'collections' => CollectionResource::collection($this->whenLoaded('collections'))
         ];
     }
