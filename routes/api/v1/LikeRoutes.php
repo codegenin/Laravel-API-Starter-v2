d931f88a8dc5b0a54cd016989a934e4ed3992159
James Puro <james.louie.puro@gmail.com>
2018-07-06 00:02:05 +0800

    Fix route

--- routes/api/v1/LikeRoutes.php
+++ routes/api/v1/LikeRoutes.php
@@ -11,7 +11,7 @@ $api->version('v1', function (Router $api) {
         $api->get('{id}/image', 'App\\ACME\\Api\\V1\\Like\\Controllers\\LikeMediaController@run');
     
         $api->get('user-likes', 'App\\ACME\\Api\\V1\\Like\\Controllers\\ListUserLikesController@run');
-        $api->get('user-books', 'App\\ACME\\Api\\V1\\Like\\Controllers\\ListUserBooksController@run');
+        $api->get('user-book', 'App\\ACME\\Api\\V1\\Like\\Controllers\\ListUserBooksController@run');
         $api->get('{id}/remove-book', 'App\\ACME\\Api\\V1\\Like\\Controllers\\RemoveBookMediaController@run');
     
     });
