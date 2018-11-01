506e00adaf4f6ca47844dd715187aae8efb2a500
James Puro <james.louie.puro@gmail.com>
2018-11-02 02:03:54 +0800

    Test

--- app/ACME/Api/V1/Collection/Controllers/ListImagesAndIsBookedController.php
+++ app/ACME/Api/V1/Collection/Controllers/ListImagesAndIsBookedController.php
@@ -117,6 +117,7 @@ class ListImagesAndIsBookedController extends ApiResponseController
                     ->where('collection_name', $collection->slug)
                     ->orderBy('created_at', 'desc')
                     ->remember(1400)
+                    ->get()
                     ->count();
     }
     
