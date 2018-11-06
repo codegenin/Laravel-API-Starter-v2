37ff09cd5ef028cffb423140bee2b47443a5afcd
James Puro <james.louie.puro@gmail.com>
2018-11-06 23:59:22 +0800

    Remove

--- app/ACME/Api/V1/Collection/Controllers/ListImagesController.php
+++ app/ACME/Api/V1/Collection/Controllers/ListImagesController.php
@@ -65,7 +65,6 @@ class ListImagesController extends ApiResponseController
         ])
                            ->where('collection_name', $collection->slug)
                            ->orderBy('created_at', 'desc')
-                           ->remember(1400)
                            ->take(1)
                            ->get();
         
@@ -91,17 +90,16 @@ class ListImagesController extends ApiResponseController
             ])
                                   ->where('collection_name', '!=', $collection->slug)
                                   ->where('model_type', '!=', 'App\Models\Category')
+                                  ->whereHas('collection', function ($query) use ($collection) {
+                                      $query->where('category_id', $collection->category_id);
+                                  })
                                   ->whereHas('translations', function ($query) use ($mainImages) {
                                       $query->orWhere('location', $mainImages[0]->location);
                                   })
                                   ->whereHas('collection.translations', function ($query) use ($mainImages) {
                                       $query->orWhere('time_period', $mainImages[0]->collection->time_period);
                                   })
-                                  ->whereHas('collection', function ($query) use ($collection) {
-                                      $query->orWhere('category_id', $collection->category_id);
-                                  })
                                   ->inRandomOrder()
-                                  ->remember(1400)
                                   ->take(3)
                                   ->get();
         }
