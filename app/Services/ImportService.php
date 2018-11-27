ed558f01a649883a88d9f689d6a79b14ee13a829
James Puro <james.louie.puro@gmail.com>
2018-11-27 20:33:11 +0800

    Add check if image is present

--- app/Services/ImportService.php
+++ app/Services/ImportService.php
@@ -12,29 +12,34 @@ class ImportService
     public static function processImportedRecord($record)
     {
         try {
-            // Check or create category
-            $categoryId = self::createOrGetCategory($record);
-            $collection = self::createOrGetCollection($record, $categoryId);
             
-            // Add media
-            $media                                    = $collection->addMediaFromUrl($record->image_url)
-                                                                   ->toMediaCollection($collection->slug);
-            $media->category_id                       = $categoryId;
-            $media->user_id                           = 1;
-            $media->museum                            = $record->museum;
-            $media->translateOrNew('en')->title_short = $record->en_title;
-            $media->translateOrNew('fr')->title_short = $record->fr_title;
-            $media->translateOrNew('en')->title       = $record->en_complete_title;
-            $media->translateOrNew('fr')->title       = $record->fr_complete_title;
-            $media->translateOrNew('en')->location    = $record->en_location;
-            $media->translateOrNew('fr')->location    = $record->fr_location;
-            $media->translateOrNew('en')->medium      = $record->en_art_medium;
-            $media->translateOrNew('fr')->medium      = $record->fr_art_medium;
-            $media->translateOrNew('en')->description = $record->credit_line;
-            $media->translateOrNew('fr')->description = $record->credit_line;
-            $media->score                             = 0;
-            $media->url                               = $record->url;
-            $media->save();
+            if (self::checkRemoteFile($record->image_url)) {
+                
+                // Check or create category
+                $categoryId = self::createOrGetCategory($record);
+                $collection = self::createOrGetCollection($record, $categoryId);
+                
+                // Add media
+                $media                                    = $collection->addMediaFromUrl($record->image_url)
+                    ->toMediaCollection($collection->slug);
+                $media->category_id                       = $categoryId;
+                $media->user_id                           = 1;
+                $media->museum                            = $record->museum;
+                $media->translateOrNew('en')->title_short = $record->en_title;
+                $media->translateOrNew('fr')->title_short = $record->fr_title;
+                $media->translateOrNew('en')->title       = $record->en_complete_title;
+                $media->translateOrNew('fr')->title       = $record->fr_complete_title;
+                $media->translateOrNew('en')->location    = $record->en_location;
+                $media->translateOrNew('fr')->location    = $record->fr_location;
+                $media->translateOrNew('en')->medium      = $record->en_art_medium;
+                $media->translateOrNew('fr')->medium      = $record->fr_art_medium;
+                $media->translateOrNew('en')->description = $record->credit_line;
+                $media->translateOrNew('fr')->description = $record->credit_line;
+                $media->score                             = 0;
+                $media->url                               = $record->url;
+                $media->save();
+            }
+            
         } catch (\Exception $e) {
             $record->imported     = 2;
             $record->import_error = $e;
@@ -44,12 +49,37 @@ class ImportService
         
     }
     
+    /**
+     * Checks if an image is present
+     *
+     * @param $url
+     * @return bool
+     */
+    private static function checkRemoteFile($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // don't download content
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        $result = curl_exec($ch);
+        curl_close($ch);
+        
+        if ($result !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     private static function createOrGetCollection($record, $category)
     {
         App::setLocale('en');
         
         $collection = Collection::whereTranslation('title', trim($record->en_collection), 'en')
-                                ->get();
+            ->get();
         
         if ($collection->count() <= 0) {
             $collection = new Collection();
@@ -77,7 +107,7 @@ class ImportService
         App::setLocale('en');
         
         $category = Category::whereTranslation('name', trim($record->en_department), 'en')
-                            ->get();
+            ->get();
         
         if ($category->count() <= 0) {
             // Create category
