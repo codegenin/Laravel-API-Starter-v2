84b40b368dbc537a8ca23d37547354ffc60a526d
James Puro <james.louie.puro@gmail.com>
2019-01-30 21:41:46 +0800

    Fix report

--- resources/views/admin/report/reports.blade.php
+++ resources/views/admin/report/reports.blade.php
@@ -7,27 +7,29 @@
         <td style="width: 10px;">{{trans('label.created_at')}}</td>
         <td style="width: 10px;">{{trans('label.actions')}}</td>
     </tr>
-    @foreach($reports as $report)
-        <tr>
-            <td>{{$report->reportable_type}}</td>
-            <td>
-                <a href="{{$report->reportable->getUrl('small')}}" target="_blank">
-                <img src="{{$report->reportable->getUrl('small')}}" alt="" width="100" height="100">
-                </a>
-            </td>
-            <td>{{!empty($report->user) ? $report->user->email : ''}}</td>
-            <td>{{$report->created_at}}</td>
-            <td>
-                <div class="btn-group">
-                    {{--<a class="btn btn-warning edit" data-id="{{$report->id}}" title="{{trans('label.edit')}}">
-                        <i class="fa fa-pencil"></i>
-                    </a>--}}
-                    <a href="#" data-id="{{$report->reportable_id}}" title="{{trans('label.delete')}}"
-                       class="btn btn-danger delete"><i class="fa fa-remove"></i></a>
-                </div>
-            </td>
-        </tr>
-    @endforeach
+    @if($reports)
+        @foreach($reports as $report)
+            <tr>
+                <td>{{$report->reportable_type}}</td>
+                <td>
+                    <a href="{{$report->reportable->getUrl('small')}}" target="_blank">
+                        <img src="{{$report->reportable->getUrl('small')}}" alt="" width="100" height="100">
+                    </a>
+                </td>
+                <td>{{!empty($report->user) ? $report->user->email : ''}}</td>
+                <td>{{$report->created_at}}</td>
+                <td>
+                    <div class="btn-group">
+                        {{--<a class="btn btn-warning edit" data-id="{{$report->id}}" title="{{trans('label.edit')}}">
+                            <i class="fa fa-pencil"></i>
+                        </a>--}}
+                        <a href="#" data-id="{{$report->reportable_id}}" title="{{trans('label.delete')}}"
+                           class="btn btn-danger delete"><i class="fa fa-remove"></i></a>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+    @endif
     </tbody>
 </table>
 @include('admin.common.delete')
